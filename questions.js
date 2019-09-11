1f2a703c652b7e4cb18198d3e54cc70a7e4eb399
Azizah Cherie Blackwood <40805624+Yurismm@users.noreply.github.com>
2019-09-11 13:51:38 +0100

    Initial commit

---
+++ questions.js
@@ -0,0 +1,45 @@
+module.exports = {
+   
+   /**
+    * I put the answer first so therefore it would be the correct one, the order of the answers should be shuffled though
+    * - Azizah 12:30 11/09/2019
+    *
+    * I noticed that you wanted there to be a picture displayed, however i'm not too sure on how to do it (for an echo spot) 
+    * - Azizah 12:41
+    * 
+    * I can also help translate the test into different languages if needed (spanish + japanese)
+    * - Azizah 12:43
+  */
+  
+  QUESTIONS_EN_GB: [
+    {
+      'Which of the following factors should you consider when trying to decide if something youve read is true or false?': [
+        'Whether or not it is reported by multiple sources',
+        'Whether your best friend shared it',
+        'Whether or not it seems likely',
+        'All of the above',
+      ],
+    },
+    {
+      'True or False: If something has gone viral on social media, its most likely true.': [
+        "False",
+        'True',
+      ],
+    },
+    {
+      'Give an example of a belief echo?': [
+        'Continuing to behave as thought you believe something that you know is false',
+        'Continuing to share information you know is false',
+        'Wanting to believe or not believe something because it fits with your world view',
+      ],
+    },
+    {
+      'Which of the following emotions are most often triggered by misinformation?': [
+        'All of the above',
+        'Disgust',
+        'Surprise',
+        'Fear',
+      ],
+    },
+  ],
+};
